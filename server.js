66067242075dc8d370cfcc30a6e2404e82e80bdb
lizdenhup <lizdenhup@gmail.com>
2017-09-27 17:19:06 -0500

    failing 2 tests, unclear errors

--- server.js
+++ server.js
@@ -1,36 +1,71 @@
 "use strict";
-let messages = [];
-
-class Message {
-  constructor(message) {
-    //not a good id generator.
-    //does not protect against collisions and is not truly random
-    //but this is just a toy project
-    this.id = Math.random() * message.length; 
-    this.message = message;
-     
-  }
-}
 
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
-const bodyParser = require('body-parser');
+const urlParser    = require('url');
+const querystring  = require('querystring');
+const bodyParser   = require('body-parser');
+const bcrypt = require('bcrypt');
 
-const router = new Router({ mergeParams: true});
+const router = new Router({ mergeParams: true });
 router.use(bodyParser.json());
 
+const saltRounds = 10;
+let messages = [];
+
+class Message {
+  constructor(input) {
+    this.id = messages.length + 1;
+    this.message = input.message;
+  }
+}
+
 router.get('/', (request, response) => {
-  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   const body = 'Hello, World!';
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
   response.end(body);
 });
 
 router.post('/message', (request, response) => {
-  let newMessage = new Message(request.body.message);
+  let newMessage = new Message(request.body);
   messages.push(newMessage);
+
   response.setHeader('Content-Type', 'application/json; charset=utf-8');
   response.end(JSON.stringify(newMessage.id));
+});
+
+router.get('/messages', (request, response) => {
+  const body = JSON.stringify(messages);
+  const url = urlParser.parse(request.url);
+  const query = querystring.parse(url.query);
+
+  if(query.encrypt) {
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    bcrypt.hash(body, saltRounds, (err, hashedBody) => {
+      response.end(hashedBody);
+    });
+  }
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  response.end(body);
+});
+
+router.get('/message/:id', (request, response) => {
+  const message = messages[request.params.id - 1];
+  const body = JSON.stringify(message);
+  const url = urlParser.parse(request.url);
+  const query = querystring.parse(url.query);
+
+  if(query.encrypt) {
+    response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    bcrypt.hash(body, saltRounds, (err, hashedBody) => {
+      response.end(hashedBody);
+    });
+  }
+
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  response.end(body);
 });
 
 const server = http.createServer((request, response) => {
