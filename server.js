2796d498cf8079b88f786fbab43801c43c5f7271
lizdenhup <lizdenhup@gmail.com>
2017-09-27 15:08:17 -0500

    4 tests passing, 4 tests failing

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 "use strict";
+let messages = [];
+
+class Message {
+  constructor(message) {
+    //not a good id generator.
+    //does not protect against collisions and is not truly random
+    //but this is just a toy project
+    this.id = Math.random() * message.length; 
+    this.message = message;
+     
+  }
+}
 
 const http         = require('http');
 const finalhandler = require('finalhandler');
 const Router       = require('router');
+const bodyParser = require('body-parser');
 
-const router = new Router();
+const router = new Router({ mergeParams: true});
+router.use(bodyParser.json());
 
 router.get('/', (request, response) => {
-  // A good place to start!
-  response.end();
+  response.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  const body = 'Hello, World!';
+  response.end(body);
+});
+
+router.post('/message', (request, response) => {
+  let newMessage = new Message(request.body.message);
+  messages.push(newMessage);
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  response.end(JSON.stringify(newMessage.id));
 });
 
 const server = http.createServer((request, response) => {
